0ec20c22df20b6d155b3b334611fced95de37fdc
don <donryu1031@gmail.com>
2023-05-25 17:21:11 -0700

    controller crud

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -14,18 +14,45 @@ namespace SuperHeroApi.Controllers
            new SuperHero{Id = 1, Name="Spider Main", FirstName = "Peter", LastName = "Parker"},
            new SuperHero{Id = 2, Name="Iron Main", FirstName = "Tony", LastName = "Stark"}
         };
-
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
             return Ok(superHeroes);
         }
-        [HttpGet]
+        [HttpGet("{id}")]
         public  async Task<ActionResult<SuperHero>> GetSingleHero(int id)
         {
-
             var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null) return NotFound("Sorry, but this hero does not exist");
             return Ok(hero);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            superHeroes.Add(hero);
+            return Ok(hero);
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(int id,SuperHero request)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null) return NotFound("Not Found");
+
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Name = request.Name;
+            hero.Place = request.Place;
+             
+            return Ok(superHeroes);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if(hero is null) return NotFound("Not Found");
+
+            superHeroes.Remove(hero);
+            return Ok(superHeroes);
         }
 
     }
